a64a187d45984c6f9155cdd870c61eec17904160
Arkansio <38956983+Arkansio@users.noreply.github.com>
2019-03-04 17:05:22 +0100

    modify go script

--- 02_bonus/01/scripts/main.go
+++ 02_bonus/01/scripts/main.go
@@ -10,5 +10,9 @@ func main() {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world")
+        })
+	fmt.Println("listening :80..")
 	http.ListenAndServe(":80", nil)
 }
